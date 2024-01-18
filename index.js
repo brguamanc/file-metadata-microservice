03652be38a7f20f9ab32a5421d9880bd64b02135
brguamanc <byrong1998@gmail.com>
2024-01-17 21:24:02 -0500

    cambio

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-var express = require('express');
-var cors = require('cors');
-var multer = require('multer');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+require("dotenv").config();
 
 var app = express();
-var storage = multer.memoryStorage();
-var upload = multer({ storage: storage });
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
 app.use((req, res, next) => {
-  console.log("method: " + req.method + "  |  path: " + req.path + "  |  IP - " + req.ip);
+  console.log(
+    "method: " + req.method + "  |  path: " + req.path + "  |  IP - " + req.ip,
+  );
   next();
 });
 
-
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-});
-
-app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
   console.log(req.file);
   if (req.file) {
     res.json({
       name: req.file.originalname,
       type: req.file.mimetype,
-      size: req.file.size
+      size: req.file.size,
     });
   } else {
     res.json("No file found");
   }
 });
 
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
